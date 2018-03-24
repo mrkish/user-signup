8f156758ba92fdeaf56102342b4c1393583c03f2
Mark Johnson <mark@jhns.in>
2018-03-24 18:04:59 -0500

    Stubbing html/main.py signup func

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, redirect, render_template
+import cgi
+import os
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+@app.route('/', methods=['POST','GET'])
+def signup():
+    
+
+    return render_template('base.html')
+
+
+
+if __name__ == "__main__":
+    app.run()
