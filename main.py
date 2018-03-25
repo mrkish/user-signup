bd7758eb242fa31f18cf89695cdaf8aabb963ba6
Mark Johnson <mark.jhns.in>
2018-03-25 18:31:38 -0500

    Starting to build out validation handling

--- main.py
+++ main.py
@@ -6,16 +6,34 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
-def verify_email():
-#    if email1 not email2:
-    return render_template('base.html')
+def verify_email(email):
+    # regex check to see that the email is valid
+    return True
 
+def verify_password(pass1, pass2):
+
+    return True 
+    
+    
 
 @app.route('/', methods=['POST','GET'])
 def signup():
-    
 
-    return render_template('base.html')
+    email_error = ''
+    password_error = ''
+    
+    if request.method == "POST":
+        user = cgi.escape(request.form['user_name'])
+        email = request.form['email']
+        pass1 = request.form['password1']
+        pass2 = request.form['passowrd2']
+
+        if (verify_email(email) == True and
+            verify_password(pass1, pass2) == True):
+            
+            return render_template('signup.html')
+
+    return render_template('signup.html')
 
 
 
